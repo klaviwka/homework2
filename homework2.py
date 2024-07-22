84a92c0009f002f8ce800d6d0a1ddcb66523f875
klaviwka <klaviwka@mail.ru>
2024-07-22 13:54:30 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+close_lesson = 12
+time_spend = 1.5
+nameOfEducation = 'Python'
+needed_Time = time_spend / close_lesson
+print(nameOfEducation, ", всего задач:", close_lesson, ", затрачено часов", time_spend, ", среднее время выполнения", needed_Time, "часа")
